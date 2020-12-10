5d98ea4ff92654a7c8253ee87d7855c7197a2fdb
이유주 <68896112+poetzu@users.noreply.github.com>
2020-12-10 14:24:45 +0900

    Add files via upload

---
+++ 1파이썬둘러보기/파이썬 기초 문법 따라 해 보기.py
@@ -0,0 +1,40 @@
+
+# 실행 단축키  ctrl + shift + F10
+
+print(1+2)
+print(3/2.4)
+print(3 * 9)
+
+a = 1
+b = 2
+print(a + b)
+
+a = "Python"
+print(a)
+
+a = 3
+if a > 1:
+    print("a is geater than 1")
+
+for a in [1,2,3]:
+    print(a)
+
+
+i = 0
+while i<3:
+    i = i + 1
+    print(i)
+
+def add(c,d):
+    return c + d
+
+
+print(add(10,100))
+
+
+
+
+
+
+
+
